97829190aa04b14bf5a82b2af513d3350fa11054
ME <60861214+eguonzy@users.noreply.github.com>
2020-05-28 16:34:07 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -10,8 +10,10 @@ let correct = document.getElementById('correct');
 let wrong = document.getElementById('wrong');
 let right = 0;
 let left = 0;
-let sort = 7;
+let sort = Math.floor(Math.random() * 10 + 10)
+console.log(sort)
 let total = 0
+submit.disabled = true
 inp.onkeyup = (event) => {
     if (event.keyCode === 13) {
 
@@ -32,60 +34,93 @@ guess.onkeyup = (event) => {
 
     }
 }
-let randomizer = Math.floor((Math.random() * 10) + 3)
+let randomizer = Math.floor((Math.random() * 50) + 5)
 submit.onclick = gamePlay
+console.log(randomizer)
 
 function start() {
     if (inp.value == "" || inp.value.match(/[1234567890]/) || inp.value.match(/[-,+*/!@#$%^&~{}=`|]/)) {
         numberDisplay.innerText = `enter name`
+
     } else {
+        submit.disabled = false
         name.innerHTML = `welcome ${inp.value}!!!`
         name.style.textTransform = `capitalize`
         inp.style.display = `none`
-
+        guess.disabled = true
         numberDisplay.innerText = randomizer;
         setTimeout(() => {
-            numberDisplay.style.display = "none"
-
-        }, 2500);
+            numberDisplay.innerText = "enter number"
+            guess.disabled = false
+            guess.focus()
+        }, 5000);
         startGame.style.display = `none`
 
 
     }
 }
 
+
+
+
 function gamePlay() {
+    /*
+        let end = setTimeout(() => {
+            numberDisplay.innerText = `passed:${right}||failed:${left}`
+            submit.disabled = true
+            guess.value = ``
+            correct.innerText = `correct:${right}`;
+        }, 10000, );*/
 
     total++
     if (guess.value == randomizer) {
 
+
+
+        console.log(`cleared`)
+        guess.value = ``
         correct.innerText = right;
         right++
         sort++
-
+        guess.disabled = true
         correct.innerText = `correct:${right}`;
-        randomizer *= 17
+        randomizer *= Math.floor(Math.random() * 10 + 10);
         numberDisplay.style.display = `block`;
         numberDisplay.innerText = randomizer;
         setTimeout(() => {
-            numberDisplay.style.display = "none"
-
-        }, 2000);
+            numberDisplay.innerText = "enter number"
+            guess.disabled = false
+            guess.focus()
+        }, 5000);
+        /* setTimeout(() => {
+             if (!submit.click()) {
+                 numberDisplay.innerText = `passed:${right}||failed:${left}`
+                 submit.disabled = true
+                 guess.value = ``
+                 correct.innerText = `correct:${right}`;
+             }
+         }, 10000, );*/
 
         console.log(sort);
     } else {
-
-        correct.innerText = right;
         numberDisplay.style.display = `block`;
         numberDisplay.innerText = `wrong answer`;
         left++
-
+        guess.value = ``
+        guess.disabled = true
         correct.innerText = `correct:${right}`;
         submit.disabled = true
-        setTimeout(() => {
+        return setTimeout(() => {
             numberDisplay.innerText = `passed:${right}||failed:${left}`
 
-        }, 1500);
+        }, 1000);
     }
+    /*
+        end = setTimeout(() => {
+            numberDisplay.innerText = `passed:${right}||failed:${left}`
+            submit.disabled = true
+            guess.value = ``
+            correct.innerText = `correct:${right}`;
+        }, 10000, );*/
 
 }
